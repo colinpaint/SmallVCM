e815f1a43f6f6c594f5326404848cb525e39ebce
colinpaint <88828347+colinpaint@users.noreply.github.com>
2022-06-23 13:40:15 +0100

    fiddle

--- src/frame.h
+++ src/frame.h
@@ -4,50 +4,56 @@
 #include <cmath>
 #include "math.h"
 
-class Frame
-{
+//{{{
+class Frame {
 public:
+  //{{{
+  Frame()
+  {
+      mX = Vec3f(1,0,0);
+      mY = Vec3f(0,1,0);
+      mZ = Vec3f(0,0,1);
+  };
+  //}}}
+  //{{{
+  Frame(
+      const Vec3f& x,
+      const Vec3f& y,
+      const Vec3f& z
+  ) :
+      mX(x),
+      mY(y),
+      mZ(z)
+  {}
+  //}}}
 
-    Frame()
-    {
-        mX = Vec3f(1,0,0);
-        mY = Vec3f(0,1,0);
-        mZ = Vec3f(0,0,1);
-    };
+  //{{{
+  void SetFromZ(const Vec3f& z)
+  {
+      Vec3f tmpZ = mZ = Normalize(z);
+      Vec3f tmpX = (std::abs(tmpZ.x) > 0.99f) ? Vec3f(0,1,0) : Vec3f(1,0,0);
+      mY = Normalize( Cross(tmpZ, tmpX) );
+      mX = Cross(mY, tmpZ);
+  }
+  //}}}
+  //{{{
+  Vec3f ToWorld(const Vec3f& a) const
+  {
+      return mX * a.x + mY * a.y + mZ * a.z;
+  }
+  //}}}
+  //{{{
+  Vec3f ToLocal(const Vec3f& a) const
+  {
+      return Vec3f(Dot(a, mX), Dot(a, mY), Dot(a, mZ));
+  }
+  //}}}
 
-    Frame(
-        const Vec3f& x,
-        const Vec3f& y,
-        const Vec3f& z
-    ) :
-        mX(x),
-        mY(y),
-        mZ(z)
-    {}
-
-    void SetFromZ(const Vec3f& z)
-    {
-        Vec3f tmpZ = mZ = Normalize(z);
-        Vec3f tmpX = (std::abs(tmpZ.x) > 0.99f) ? Vec3f(0,1,0) : Vec3f(1,0,0);
-        mY = Normalize( Cross(tmpZ, tmpX) );
-        mX = Cross(mY, tmpZ);
-    }
-
-    Vec3f ToWorld(const Vec3f& a) const
-    {
-        return mX * a.x + mY * a.y + mZ * a.z;
-    }
-
-    Vec3f ToLocal(const Vec3f& a) const
-    {
-        return Vec3f(Dot(a, mX), Dot(a, mY), Dot(a, mZ));
-    }
-
-    const Vec3f& Binormal() const { return mX; }
-    const Vec3f& Tangent () const { return mY; }
-    const Vec3f& Normal  () const { return mZ; }
+  const Vec3f& Binormal() const { return mX; }
+  const Vec3f& Tangent () const { return mY; }
+  const Vec3f& Normal  () const { return mZ; }
 
 public:
-
-    Vec3f mX, mY, mZ;
-};
+  Vec3f mX, mY, mZ;
+  };
+//}}}
--- src/ray.h
+++ src/ray.h
@@ -4,37 +4,27 @@
 #include <cmath>
 #include "math.h"
 
-//////////////////////////////////////////////////////////////////////////
-// Ray casting
-struct Ray
-{
-    Ray()
-    {}
+//{{{
+struct Ray {
+  Ray() {}
 
-    Ray(const Vec3f& aOrg,
-        const Vec3f& aDir,
-        float aTMin
-    ) :
-        org(aOrg),
-        dir(aDir),
-        tmin(aTMin)
-    {}
+  Ray (const Vec3f& aOrg, const Vec3f& aDir, float aTMin ) :
+       org(aOrg), dir(aDir), tmin(aTMin) {}
 
-    Vec3f org;  //!< Ray origin
-    Vec3f dir;  //!< Ray direction
-    float tmin; //!< Minimal distance to intersection
-};
+  Vec3f org;  //!< Ray origin
+  Vec3f dir;  //!< Ray direction
+  float tmin; //!< Minimal distance to intersection
+  };
+//}}}
+//{{{
+struct Isect {
 
-struct Isect
-{
-    Isect()
-    {}
+  Isect() {}
+  Isect (float aMaxDist) : dist(aMaxDist) {}
 
-    Isect(float aMaxDist):dist(aMaxDist)
-    {}
-
-    float dist;    //!< Distance to closest intersection (serves as ray.tmax)
-    int   matID;   //!< ID of intersected material
-    int   lightID; //!< ID of intersected light (if < 0, then none)
-    Vec3f normal;  //!< Normal at the intersection
-};
+  float dist;    //!< Distance to closest intersection (serves as ray.tmax)
+  int   matID;   //!< ID of intersected material
+  int   lightID; //!< ID of intersected light (if < 0, then none)
+  Vec3f normal;  //!< Normal at the intersection
+  };
+//}}}
